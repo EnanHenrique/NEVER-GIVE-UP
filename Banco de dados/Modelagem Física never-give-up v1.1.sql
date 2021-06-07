98be4d980256d079daf55da5f06985422f351c24
unknown <enan.oliveira@bandtec.com.br>
2021-06-07 16:08:30 -0300

    Alterações finais

--- Banco de dados/Modelagem Física never-give-up v1.1.sql
+++ Banco de dados/Modelagem Física never-give-up v1.1.sql
@@ -13,7 +13,8 @@ create table Usuario (
     dataNascimento DATE
 );
 
-insert into Usuario values (null, 'Enan Oliveira', 'enan.oliveira', 'senhaenan', 'enan.oliveira@bandtec.com.br', '2000-02-08');
+insert into Usuario values (null, 'Enan Oliveira', 'enan.oliveira', 'senhaenan', 'enan.oliveira@bandtec.com.br', '2000-02-08'),
+						   (null, 'Silvia Oliveira', 'silvia.oliveira', 'senhasilvia', 'silvia.oliveira@gmail.com', '1960-08-07');
 
 create table AutoConhecimento (
 	idAutoConhecimento int primary key auto_increment,
@@ -59,13 +60,22 @@ create table opiniao_usuario (
     foreign key (fkUsuario) references Usuario (idUsuario)
 ) auto_increment = 200;
 
-create table estado_emocional_opinao_usuario (
+create table EstadoEmocinalUsuario (
 	fkEstadoEmocional int,
-    fkOpiniaoUsuario int,
-    dataInsercao date,
-    primary key (fkEstadoEmocional, fkOpiniaoUsuario),
+    fkUsuario int,
+    dataEmocinal date,
+    primary key (fkEstadoEmocional, fkUsuario),
     foreign key (fkEstadoEmocional) references estado_emocional (idEstadoEmocional),
-    foreign key (fkOpiniaoUsuario) references opiniao_usuario (idOpiniaoUsuario)
+    foreign key (fkUsuario) references Usuario(idUsuario)
+);
+
+create table OpiniaoUsuario (
+	fkOpiniaoUsuario int,
+    fkUsuario int,
+    dataOpiniao date,
+    primary key (fkOpiniaoUsuario, fkUsuario),
+    foreign key (fkOpiniaoUsuario) references opiniao_usuario(idOpiniaoUsuario),
+    foreign key (fkUsuario) references Usuario(idUsuario)
 );
 
 insert into AutoConhecimento values 
@@ -75,7 +85,7 @@ insert into AutoConhecimento values
     'Solidão é algo que me deixa para baixo um pouco',
     'É algo que nao me afeta muito também por não me apegar muito as pessoas',
     'Aproveito sozinho a felicidade o maximo que posso',
-    'Gera a felicidade em que gera um orgulho também', 1
+    'Gera a felicidade em que gera um orgulho também', 2
 );
 
 insert into PessoaMelhor values
@@ -85,7 +95,7 @@ insert into PessoaMelhor values
     'Esquecer a solidão e saber que tenho vários companheiros sensacionais nessa caminhada',
     'Saber que tudo tem o seu tempo',
     'Agradecer a Deus e a todos pela felicidade que estou no momento',
-    'Agradecer a todos que me ajudaram na conquista e reconhecê-los', 1
+    'Agradecer a todos que me ajudaram na conquista e reconhecê-los', 2
 );
 
 
@@ -102,6 +112,10 @@ insert into PessoaMelhor values
  
  select * from estado_emocional_opinao_usuario;
 
- select * from Usuario inner join AutoConhecimento on fkUsuario = idUsuario limit 1; -- Usuarios e seus respectivos auto onhecimentos
+ select * from Usuario inner join AutoConhecimento on fkUsuario = idUsuario ; -- Usuarios e seus respectivos auto onhecimentos
 
- select * from Usuario inner join PessoaMelhor on fkUsuario = idUsuario limit 1; -- Usuarios e suas respectivas versões de melhores pessoas
+ select * from Usuario inner join PessoaMelhor on fkUsuario = idUsuario ; -- Usuarios e suas respectivas versões de melhores pessoas
+
+select * from Usuario inner join estado_emocional on fkUsuario = idUsuario; -- Usuarios e seus respectivos estados emocionais
+
+select * from Usuario inner join opiniao_usuario on fkUsuario = idUsuario; -- Usuarios e suas respectivas opinioes sobre o site em notas de 0 a 5
